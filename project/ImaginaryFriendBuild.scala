96db7421c68fc2cd77aad6f3efe15df60965a324
Sam Halliday <sam.halliday@gmail.com>
2016-02-10 19:27:00 +0000

    licence and basic build

---
+++ project/ImaginaryFriendBuild.scala
@@ -0,0 +1,16 @@
+// Copyright 2016 Sam Halliday
+// Licence: http://www.apache.org/licenses/LICENSE-2.0
+import sbt._
+import Keys._
+import SonatypeSupport._
+
+object ImaginaryFriendBuild extends Build {
+  lazy val root = Project("imaginary-friend", file(".")).
+    settings(Sensible.settings).
+    settings(sonatype("fommil", "imaginary-friend", Apache2)).
+    settings(
+      libraryDependencies += "org.typelevel" %% "macro-compat" % "1.1.0",
+      addCompilerPlugin("org.scalamacros" %% "paradise" % "2.1.0" cross CrossVersion.full),
+      updateOptions := updateOptions.value.withCachedResolution(true)
+    )
+}
